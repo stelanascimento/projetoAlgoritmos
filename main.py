5a9fae255614e1bfbedae223f39b5950fcbd97d1
pedro664 <121588519+pedro664@users.noreply.github.com>
2024-07-06 20:17:34 -0300

    criando grafo

---
+++ main.py
@@ -0,0 +1,10 @@
+class Grafo:
+    def __init__(self, n):
+        self.m = n
+        self.v_dk = [[-1 for _ in range(self.m)]for _ in range(self.m)]
+
+    def add_aresta(self, u, v, d):
+        self.v_dk[u][v] = d
+        self.v_dk[v][u] = d
+
+MAX = 10**9
